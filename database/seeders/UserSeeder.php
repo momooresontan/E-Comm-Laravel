af733a675b9ae9090dfacdb177474bc4b6e89a56
Momooreoluwa Sontan <sontanmomo@gmail.com>
2024-02-13 20:18:24 +0100

    user controller added

--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -4,6 +4,8 @@ namespace Database\Seeders;
 
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Hash;
 
 class UserSeeder extends Seeder
 {
@@ -12,6 +14,10 @@ class UserSeeder extends Seeder
      */
     public function run(): void
     {
-        //
+        DB::table('users')->insert([
+            'name' => 'Peter Parker',
+            'email' => 'pparker@test.io',
+            'password' => Hash::make('password'),
+        ]);
     }
 }
