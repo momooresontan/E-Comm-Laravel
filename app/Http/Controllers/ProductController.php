dd9ddbdd3c2697f31423e21a47374e5fb412eb59
Momooreoluwa Sontan <sontanmomo@gmail.com>
2024-02-15 16:28:45 +0100

    product model added

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -3,10 +3,10 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
-
+use App\Models\Product;
 class ProductController extends Controller
 {
     public function index(){
-        return "Welcome to product page";
+        return Product::all();
     }
 }
