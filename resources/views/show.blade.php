2b0b4f70a5f65c39cff93eb7a4b25916a1fc47bf
Momooreoluwa Sontan <sontanmomo@gmail.com>
2024-02-19 18:55:54 +0100

    show blade modified

--- resources/views/show.blade.php
+++ resources/views/show.blade.php
@@ -8,6 +8,14 @@
             </div>
             <div class="col-sm-6">
                 <a href="/"><i class="bi bi-arrow-left"></i>Go Back</a>
+                <h2>{{ $product->name }}</h2>
+                <h3>Price: {{ $product->price }}</h3>
+                <h4>Details: {{ $product->description }}</h4>
+                <h4>Category: {{ $product->category }}</h4>
+                <br><br>
+                <button class="btn btn-primary text-uppercase">Add to Cart</button>
+                <button class="btn btn-success text-uppercase m-3">Buy Now</button>
+                <br><br>
             </div>
         </div>
     </div>
