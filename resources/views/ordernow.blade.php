8edda88b81099e8f4127c7cd965d80d6738aadf6
Momooreoluwa Sontan <sontanmomo@gmail.com>
2024-02-20 14:40:45 +0100

    product controller modified & ordernow blade modified

--- resources/views/ordernow.blade.php
+++ resources/views/ordernow.blade.php
@@ -1,11 +1,46 @@
 @extends('layout')
 
 @section('content')
-
-<div class="custom-product">
-    <div class="col-sm-10">
-        
+    <div class="custom-product">
+        <div class="col-sm-10">
+            <table class="table">
+                <tbody>
+                    <tr>
+                        <td>Amount</td>
+                        <td>$ {{ $total }}</td>
+                    </tr>
+                    <tr>
+                        <td>Tax</td>
+                        <td>$ 0</td>
+                    </tr>
+                    <tr>
+                        <td>Delivery</td>
+                        <td>$ 10</td>
+                    </tr>
+                    <tr>
+                        <td>Total Amount</td>
+                        <td>$ {{ $total + 10 }}</td>
+                    </tr>
+                </tbody>
+            </table>
+            <div>
+                <form action="/action_page.php">
+                    <div class="mb-3 mt-3">
+                        <textarea type="email" class="form-control" id="email" placeholder="Enter email" name="email">
+                    </div>
+                    <div class="mb-3">
+                        <label for="pwd" class="form-label">Password:</label>
+                        <input type="password" class="form-control" id="pwd" placeholder="Enter password"
+                            name="pswd">
+                    </div>
+                    <div class="form-check mb-3">
+                        <label class="form-check-label">
+                            <input class="form-check-input" type="checkbox" name="remember"> Remember me
+                        </label>
+                    </div>
+                    <button type="submit" class="btn btn-primary">Submit</button>
+                </form>
+            </div>
+        </div>
     </div>
-</div>
-
 @endsection
