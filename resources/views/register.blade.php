bf3bb1d35d79914ee8910da3e2ccc4450a2da595
Momooreoluwa Sontan <sontanmomo@gmail.com>
2024-02-21 12:48:57 +0100

    register functionality

--- resources/views/register.blade.php
+++ resources/views/register.blade.php
@@ -4,7 +4,7 @@
     <div class="container custom-login">
         <div class="row">
             <div class="col-sm-4 col-sm-offset-4 mx-auto">
-                <form method="POST" action="/login">
+                <form method="POST" action="/register">
                     @csrf
                     <div class="mb-3">
                         <label for="name" class="form-label">Name</label>
@@ -19,7 +19,7 @@
                         <label for="exampleInputPassword1" class="form-label">Password</label>
                         <input type="password" name="password" class="form-control" id="exampleInputPassword1">
                     </div>
-                    <button type="submit" class="btn btn-primary">Login</button>
+                    <button type="submit" class="btn btn-primary">Register</button>
                 </form>
             </div>
         </div>
